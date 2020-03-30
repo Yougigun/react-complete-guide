ccd18cabe493e38e37b41a2ca9ccb53709d450dd
Yougigun <s14031403@gmail.com>
2020-03-30 15:57:41 +0800

    48. Adding Styling with Stylesheets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import "./Person.css";
 const person = ( props ) => {
     return (
-        <div>
+        <div className="Person" >
             <p onClick={props.click}> I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             {/* Set onChange to set new state and value to show from start */}
