d59edfd660e35cfefa6ca6708c057d994f192278
Yougigun <s14031403@gmail.com>
2020-06-13 16:49:38 +0800

    58. Lists & State

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
+
 class App extends Component {
   state = {
     persons: [
@@ -12,18 +13,13 @@ class App extends Component {
     otherState: 'some other value'
   };
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ],
-      showPersons:false
-    });
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex,1)
+    this.setState({persons:persons})
+
   };
+
 
   // switchNameHandler(newName)  {
   //   // console.log('Was clicked!');
@@ -68,10 +64,11 @@ class App extends Component {
     if (this.state.showPersons === true){
       persons=(
         <div >
-          {this.state.persons.map(person =>{
+          {this.state.persons.map((person,index) =>{
             return <Person 
-                   name={person.name}
-                   age = {person.age} />
+              click = {()=>this.deletePersonHandler(index)}
+              name={person.name}
+              age = {person.age} />
           })}
       </div>
       );
